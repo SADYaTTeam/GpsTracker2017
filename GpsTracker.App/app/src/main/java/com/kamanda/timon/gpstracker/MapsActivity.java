b3e9f5e0720a7c94e58e86e06b3ca6df8b4bc823
marta <darusia97@ukr.net>
2017-05-29 11:50:58 +0300

    deleted unused functions

--- GpsTracker.App/app/src/main/java/com/kamanda/timon/gpstracker/MapsActivity.java
+++ GpsTracker.App/app/src/main/java/com/kamanda/timon/gpstracker/MapsActivity.java
@@ -141,7 +141,7 @@ public class MapsActivity extends FragmentActivity
     protected void onStart() {
         googleApiClient.connect();
         super.onStart();
-        minimizeApp();
+       // minimizeApp();
     }
 
     /**
@@ -177,8 +177,8 @@ public class MapsActivity extends FragmentActivity
 
         // Add a marker in Lviv and move the camera
 
-      //  LatLng latLng = new LatLng(49.840466, 24.027845);
-        LatLng latLng = new LatLng(message.getLatitude(), message.getLongtitude());
+        LatLng latLng = new LatLng(49.840466, 24.027845);
+        //LatLng latLng = new LatLng(message.getLatitude(), message.getLongtitude());
         mMap.addMarker(new MarkerOptions().position(latLng).title("Current Position"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
         mMap.animateCamera(CameraUpdateFactory.zoomTo(ZOOM));
@@ -204,7 +204,7 @@ public class MapsActivity extends FragmentActivity
         mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
         mMap.animateCamera(CameraUpdateFactory.zoomTo(ZOOM));
         mMap.getUiSettings().setZoomControlsEnabled(true);
-        saveLocationToFile();
+
 
 
     }
@@ -223,7 +223,7 @@ public class MapsActivity extends FragmentActivity
     @Override
     public void onConnected(final @Nullable Bundle bundle) {
 
-        getCurrentLocation();
+
         moveMap();
     }
 
@@ -292,223 +292,14 @@ public class MapsActivity extends FragmentActivity
      *
      */
     //Get current location
-    private void getCurrentLocation() {
-        mMap.clear();
-        if (ActivityCompat.checkSelfPermission(this,
-                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-        Location location = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
-        if (location != null) {
-            //Getting longitude and latitude
-            message.setLongitude(location.getLongitude());
-            message.setLatitude(location.getLatitude());
-            Log.i("GPS_Coordinates_Start", message.getLatitude() + "; " + message.getLongtitude());
 
-            //moving the map to location
-            moveMap();
-        }
-    }
 
     /**
      * @return
      */
-    private String getCurrentLocationString() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-        }
-        Location location = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
-        if (location != null) {
-            //Getting longitude and latitude
-            message.setLongitude(location.getLongitude());
-            message.setLatitude(location.getLatitude());
-        }
-        return message.getLatitude() + "; " + message.getLongtitude();
-    }
+
     //endregion Get current location
 
-    //region Save location to file
-
-    /**
-     *
-     */
-    //Saving current location to file
-    private void saveLocationToFile() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-
-            String filename = "MyLocation.txt";
-//            String deviceId = Secure.getString(this.getContentResolver(),
-//                Secure.ANDROID_ID);
-
-            String sData = getCurrentLocationString() + " \nDevice id:" + deviceId;
-
-
-        String root = Environment.getExternalStorageDirectory().toString();
-        File myDir = new File(root + "/savedCoordinates");
-        try {
-            myDir.mkdirs();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        File file = new File(myDir, filename);
-        try {
-            if (file.exists()) {
-                try {
-                    file.delete();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        try {
-            FileOutputStream out = new FileOutputStream(file);
-            out.write(sData.getBytes());
-            out.flush();
-            out.close();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    //endregion Save location to file
-
-    //region Generate JSON and send it to URL
-
-    /**
-     *
-     */
-    //check if you are connected or not
-    private void checkConnection() {
-        try {
-            if (isConnected()) {
-                Toast.makeText(getBaseContext(), "You are connected", Toast.LENGTH_LONG).show();
-            } else {
-                Toast.makeText(getBaseContext(), "You are NOT connected", Toast.LENGTH_LONG).show();
-            }
-        } catch (Exception ex) {
-
-        }
-
-    }
-
-    /**
-     * @param url
-     * @param message
-     * @return
-     */
-    public static String post(final String url, final DataMessage message) {
-        InputStream inputStream = null;
-        String result = "";
-        try {
-
-            // 1. create HttpClient
-            HttpClient httpclient = new DefaultHttpClient();
-
-            // 2. make post request to the given URL
-            HttpPost httpPost = new HttpPost(url);
-
-            String json = "";
-
-            // 3. build jsonObject
-            JSONObject jsonObject = new JSONObject();
-            jsonObject.accumulate("longtitude", message.getLongtitude());
-            jsonObject.accumulate("latitude", message.getLatitude());
-            jsonObject.accumulate("deviceId", message.getDeviceId());
-
-            // 4. convert JSONObject to JSON to String
-            json = jsonObject.toString();
-//          jsonSave = json;
-
-
-            // ** Alternative way to convert Person object to JSON string usin Jackson Lib
-            // ObjectMapper mapper = new ObjectMapper();
-            // json = mapper.writeValueAsString(person);
-
-            // 5. set json to StringEntity
-            StringEntity se = new StringEntity(json);
-
-            // 6. set httpPost Entity
-            httpPost.setEntity(se);
-
-            // 7. Set some headers to inform server about the type of the content
-            httpPost.setHeader("Accept", "application/json");
-            httpPost.setHeader("Content-type", "application/json");
-
-            // 8. Execute post request to the given URL
-            HttpResponse httpResponse = httpclient.execute(httpPost);
-
-            // 9. receive response as inputStream
-            inputStream = httpResponse.getEntity().getContent();
-
-            // 10. convert inputstream to string
-            if (inputStream != null) {
-                result = convertInputStreamToString(inputStream); }
-            else {
-                result = "Did not work!";
-            }
-        } catch (Exception e) {
-            Log.d("InputStream", e.getLocalizedMessage());
-        }
-
-        // 11. return result
-        return result;
-    }
-
-    /**
-     * @return
-     */
-    public boolean isConnected() {
-        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Activity.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-        if (networkInfo != null && networkInfo.isConnected()) {
-            return true; }
-        else {
-            return false;
-        }
-    }
-
-    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
-        @Override
-        protected String doInBackground(final String... urls) {
-
-            return post(urls[0], mapListener.getDataMessage());
-        }
-        // onPostExecute displays the results of the AsyncTask.
-        @Override
-        protected void onPostExecute(final String result) {
-            Toast.makeText(getBaseContext(), "Data Sent!", Toast.LENGTH_LONG).show();
-        }
-    }
-
-    private static String convertInputStreamToString(final InputStream inputStream) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-        String line = "";
-        String result = "";
-        StringBuffer buf = new StringBuffer();
-        while ((line = bufferedReader.readLine()) != null) {
-            buf.append(line);
-        }
-        result = buf.toString();
-        inputStream.close();
-        return result;
-
-    }
-
-    //TODO Complete validate method DataMessage object and place
-//    private boolean validate(){
-//        if(message.getLatitude() ==  Double.isNaN() )
-//            return false;
-//        else if(etCountry.getText().toString().trim().equals(""))
-//            return false;
-//        else if(etTwitter.getText().toString().trim().equals(""))
-//            return false;
-//        else
-//            return true;
-    //endregion SendJSON
 
     /**
      * @param keyCode
@@ -521,9 +312,8 @@ public class MapsActivity extends FragmentActivity
         switch (keyCode) {
             case KeyEvent.KEYCODE_VOLUME_UP:
                 event.startTracking();
-                saveLocationToFile();
-                //TODO Input Server URL here in future
-                new HttpAsyncTask().execute("172.16.0.88");
+
+
                 return true;
             default: break;
         }
